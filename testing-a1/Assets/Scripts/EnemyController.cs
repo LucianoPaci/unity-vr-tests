0256afe2edfc4f989ac37c3a786d5adf777e9f46
Paci <lucianopaci93@gmail.com>
2021-04-15 09:01:08 -0300

    Added New Scene. Updated Menu Prefabs

---
+++ testing-a1/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 3f;
+    void Start()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Ball"))
+        {
+            Destroy(gameObject); // Destroy ourself
+        }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject); // Destroy ourself
+            Debug.Log("KILLED");
+
+        }
+    }
+}
---
+++ testing-a1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public Transform ballTransform;
+    public float speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("BottomTrigger") || Input.GetButtonDown("Fire1"))
+        {
+            GameObject tmpBall =  Instantiate(ballPrefab, ballTransform.position, ballTransform.rotation);
+            tmpBall.GetComponent<Rigidbody>().velocity = ballTransform.forward * speed;
+            
+        }
+    }
+}
