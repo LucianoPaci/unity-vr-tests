e6cabc29a6f199ada52d8cb367c8f169a4d52712
Paci <lucianopaci93@gmail.com>
2021-04-15 22:57:39 -0300

    Added Testing Scene for EVENTS

---
+++ testing-a1/Assets/Scripts/ClickPlane.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClickPlane : MonoBehaviour
+{
+
+    public static Action OnPlaneClicked = null;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0) || Input.GetButtonDown("Fire1") || Input.GetButtonDown("BottomTrigger"))
+        {
+            OnPlaneClicked();
+        }
+        
+    }
+}
---
+++ testing-a1/Assets/Scripts/Events/ChangePosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePosition : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        ClickPlane.OnPlaneClicked += MoveUp;
+    }
+
+    private void OnDisable()
+    {
+        ClickPlane.OnPlaneClicked -= MoveUp;
+    }
+
+    void MoveUp()
+    {
+        transform.position = Vector3.up;
+    }
+}
+
